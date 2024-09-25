637c9075b28ea6f9fabe128a63b9def60654b068
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-09-26 00:53:26 +0700

    Tong: Init branch

--- srcs/react-app/src/components/navbar/navbar.js
+++ srcs/react-app/src/components/navbar/navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import '../styles/styles.css';
 import { FaSignOutAlt, FaBell, FaUserCircle } from 'react-icons/fa';
 import logo from '../../assets/MUT_LOGO_crop.png';
@@ -7,25 +8,27 @@ function Navbar() {
 	return (
 		<nav className="navbar">
 			<div className="navbar-left">
-				<a href="#home" className="logo">
+				<a href="../homepage/homapage.js" className="logo">
 					<img src={logo} alt="MUT Logo" />
 				</a>
-
 				<ul className="navbar-links">
-					{['Reserve', 'History', 'Management', 'Master', 'Report'].map(item => (
-						<li key={item}><a href={`#${item.toLowerCase()}`}>{item}</a></li>
-					))}
+					<li><a href="#reserve">Reserve</a></li>
+					<li><a href="#history">History</a></li>
+					<li className="dropdown">
+						<a href="#management">Management</a>
+						<ul className="dropdown-menu">
+							<li><a href="#User">User</a></li>
+							<li><a href="#Room">Room</a></li>
+						</ul>
+					</li>
+					<li><a href="#master">Master</a></li>
+					<li><a href="#report">Report</a></li>
 				</ul>
 			</div>
-
 			<div className="navbar-icons">
-				{[
-					{ href: '#signout', icon: <FaSignOutAlt /> },
-					{ href: '#notifications', icon: <FaBell /> },
-					{ href: '#profile', icon: <FaUserCircle /> },
-				].map(({ href, icon }) => (
-					<a key={href} href={href} className="navbar-icon">{icon}</a>
-				))}
+				<a href="#signout" className="navbar-icon"><FaSignOutAlt /></a>
+				<a href="#notifications" className="navbar-icon"><FaBell /></a>
+				<a href="#profile" className="navbar-icon"><FaUserCircle /></a>
 			</div>
 		</nav>
 	);
