727d4b029249727da3850da604c4fd84170cf9d6
UTORUcy <cyutoru@gmail.com>
2024-10-26 15:17:04 +0700

    ProfileInfo & bugfix Login

--- srcs/react-app/src/components/login/login.jsx
+++ srcs/react-app/src/components/login/login.jsx
@@ -1,55 +1,55 @@
-import React,{ useState } from "react";
-import "./login.css";
-import { Link } from 'react-router-dom'
-import Logofull from "../../assets/Fulllogo.png";
+  import React,{ useState,useEffect } from "react";
+  import "./login.css";
+  import { Link } from 'react-router-dom'
+  import Logofull from "../../assets/Fulllogo.png";
 
-function Login() {
+  function Login() {
 
-   const [inputUsername, setInputUsername] = useState('');
-   const [inputPassword, setInputPassword] = useState('');
+    const [inputUsername, setInputUsername] = useState('');
+    const [inputPassword, setInputPassword] = useState('');
 
-   const handleSubmit = (e) => {
-     e.preventDefault();
+    const handleSubmit = (e) => {
+      e.preventDefault();
 
-     const buttonName = e.nativeEvent.submitter.name;
+      const buttonName = e.nativeEvent.submitter.name;
 
-     if (buttonName === 'signin') {
-      if (inputUsername.trim() === '' || inputPassword.trim() === ''){
-        alert('Please input Username or Password');
+      if (buttonName === 'signin') {
+        if (inputUsername.trim() === '' || inputPassword.trim() === ''){
+          alert('Please input Username or Password');
+        }
+        else{
+          window.location.href = '/';
+        }
+      } 
+      if (buttonName === 'forgot') {
+          alert('i forgot my password');
       }
-      else{
-        window.location.href = '/';
+      else {
       }
-     } 
-     if (buttonName === 'forgot') {
-        alert('i forgot my password');
-    }
-     else {
-    }
-   };
+    };
 
-	return (
-		<>
-  <div className="bglogin">
-      <div className="container-login">
-        <img src={Logofull} alt="Logofull" className="logofull-img" />
-        <div className="box-login">
-          <div className="welcome-login"> <b>Welcome Back</b> </div>
-          <div className="text_info-login"> Username </div>
-          <form onSubmit={handleSubmit}>
-          <input type="text" value={inputUsername} onChange={(e)=> setInputUsername(e.target.value)} className="info-login"></input>
-          <div className="text_info-login"> Password </div>
-          <input type="text" value={inputPassword} onChange={(e)=> setInputPassword(e.target.value)} className="info-login"></input>
-          <button className="forgot-login" type="submit" name="forgot"> i forgot my password </button>
-          <button className="signin-login" type="submit" name="signin"> Signin</button>
-          </form>
+    return (
+      <>
+    <div className="bglogin">
+        <div className="container-login">
+          <img src={Logofull} alt="Logofull" className="logofull-img" />
+          <div className="box-login">
+            <div className="welcome-login"><b>Welcome Back</b></div>
+            <div className="text_info-login"> Username </div>
+            <form onSubmit={handleSubmit}>
+            <input type="text" value={inputUsername} onChange={(e)=> setInputUsername(e.target.value)} className="info-login"></input>       
+            <div className="text_info-login"> Password </div>
+            <input type="text" value={inputPassword} onChange={(e)=> setInputPassword(e.target.value)} className="info-login"></input>
+            <button className="forgot-login" type="submit" name="forgot"> i forgot my password </button>
+            <button className="signin-login" type="submit" name="signin"> Signin</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
 
-			{/* <Link to="/">Login</Link> */}
-		</>
-	);
-}
+        {/* <Link to="/">Login</Link> */}
+      </>
+    );
+  }
 
-export default Login;
+  export default Login;
--- srcs/react-app/src/components/profile/profile.jsx
+++ srcs/react-app/src/components/profile/profile.jsx
@@ -1,15 +1,65 @@
-import React from "react";
-import Nav from '../navbar/navbar'
+import React, { useEffect } from "react";
+import Nav from "../navbar/navbar";
+import "./profile.css";
 
 function Profile() {
-	return (
-		<>
-			<Nav />
-			<div>
-				<h1>Profile</h1>
-			</div>
-		</>
-	);
+  // useEffect(() => {
+
+  // 	document.body.style.backgroundColor = 'rgb(155, 155, 155)'; // เปลี่ยนสี background
+
+  // 	return () => {
+  // 	  document.body.style.backgroundColor = ''; // รีเซ็ตสีเมื่อออกจากหน้านี้
+  // 	};
+  //   }, []);
+
+  return (
+    <>
+      <Nav />
+      <body>
+        <div className="bgprofile">
+          <div className="container-profile">
+            <div className="top_text-profile">
+              <b>Profile Infomation</b>
+            </div>
+            <div className="head-profile">
+              <div className="head_img-profile"></div>
+              <p className="head_name-profile">UTORU</p>
+            </div>
+            <div className="body-profile">
+              <div className="body_left-profile">
+                <div className="body_name-profile">
+                  <p>FristName :</p>
+                  <p className="body_name_input-profile">UTORU</p>
+                </div>
+                <div className="body_name-profile">
+                  <p>Department :</p>
+                  <p className="body_name_input-profile">MII</p>
+                </div>
+                <div className="body_name-profile">
+                  <p>Position :</p>
+                  <p className="body_name_input-profile">Tester</p>
+                </div>
+              </div>
+              <div className="body_right-profile">
+                <div className="body_name-profile">
+                  <p>LastName :</p>
+                  <p className="body_name_input-profile">Cy</p>
+                </div>
+                <div className="body_name-profile">
+                  <p>Point :</p>
+                  <p className="body_name_input-profile">3</p>
+                </div>
+                <div className="body_name-profile">
+                  <p>Status :</p>
+                  <p className="body_name_input-profile">Working</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </>
+  );
 }
 
 export default Profile;
