fde634760c35cccfd3d6f461e9b2fae47377058a
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-10-28 17:09:18 +0700

    Tong: Style Login

--- srcs/react-app/src/components/login/login.jsx
+++ srcs/react-app/src/components/login/login.jsx
@@ -1,55 +1,61 @@
-  import React,{ useState,useEffect } from "react";
-  import "./login.css";
-  import { Link } from 'react-router-dom'
-  import Logofull from "../../assets/Fulllogo.png";
+import React, { useState } from "react";
+import "./login.css";
+import Logofull from "../../assets/Fulllogo.png";
 
-  function Login() {
+function Login() {
+	const [inputUsername, setInputUsername] = useState("");
+	const [inputPassword, setInputPassword] = useState("");
 
-    const [inputUsername, setInputUsername] = useState('');
-    const [inputPassword, setInputPassword] = useState('');
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const buttonName = e.nativeEvent.submitter.name;
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
+		if (buttonName === "signin") {
+			if (inputUsername.trim() === "" || inputPassword.trim() === "") {
+				alert("Please input Username or Password");
+			} else {
+				window.location.href = "/";
+			}
+		}
+		if (buttonName === "forgot") {
+			alert("I forgot my password");
+		}
+	};
 
-      const buttonName = e.nativeEvent.submitter.name;
+	return (
+		<div className="bglogin">
+			<div className="container-login">
+				<img src={Logofull} alt="Logofull" className="logofull-img" />
+				<div className="box-login">
+					<h1 className="welcome-login">Welcome Back</h1>
+					<form onSubmit={handleSubmit}>
+						<label className="text_info-login">Username</label>
+						<input
+							type="text"
+							value={inputUsername}
+							onChange={(e) => setInputUsername(e.target.value)}
+							className="info-login"
+							placeholder="Enter your username"
+						/>
+						<label className="text_info-login">Password</label>
+						<input
+							type="password"
+							value={inputPassword}
+							onChange={(e) => setInputPassword(e.target.value)}
+							className="info-login"
+							placeholder="Enter your password"
+						/>
+						<button className="forgot-login" type="button" name="forgot" onClick={handleSubmit}>
+							I forgot my password
+						</button>
+						<button className="signin-login" type="submit" name="signin">
+							Sign In
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
 
-      if (buttonName === 'signin') {
-        if (inputUsername.trim() === '' || inputPassword.trim() === ''){
-          alert('Please input Username or Password');
-        }
-        else{
-          window.location.href = '/';
-        }
-      } 
-      if (buttonName === 'forgot') {
-          alert('i forgot my password');
-      }
-      else {
-      }
-    };
-
-    return (
-      <>
-    <div className="bglogin">
-        <div className="container-login">
-          <img src={Logofull} alt="Logofull" className="logofull-img" />
-          <div className="box-login">
-            <div className="welcome-login"><b>Welcome Back</b></div>
-            <div className="text_info-login"> Username </div>
-            <form onSubmit={handleSubmit}>
-            <input type="text" value={inputUsername} onChange={(e)=> setInputUsername(e.target.value)} className="info-login"></input>       
-            <div className="text_info-login"> Password </div>
-            <input type="text" value={inputPassword} onChange={(e)=> setInputPassword(e.target.value)} className="info-login"></input>
-            <button className="forgot-login" type="submit" name="forgot"> i forgot my password </button>
-            <button className="signin-login" type="submit" name="signin"> Signin</button>
-            </form>
-          </div>
-        </div>
-      </div>
-
-        {/* <Link to="/">Login</Link> */}
-      </>
-    );
-  }
-
-  export default Login;
+export default Login;
