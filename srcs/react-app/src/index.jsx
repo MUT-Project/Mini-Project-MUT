fe974262934e09f12c484eeda48067ba7a019e56
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-10-28 18:32:11 +0700

    Update Booking, Reserve, Mylists

--- srcs/react-app/src/index.jsx
+++ srcs/react-app/src/index.jsx
@@ -19,73 +19,84 @@ import Department from './components/master/department';
 import Position from './components/master/position';
 import Status from './components/master/status';
 
+import Booking from "./components/reserve/booking";
+import Mylist from "./components/reserve/mylists";
+
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-      path: "/user",
-      element: <User />
-  },
-  {
-      path: "/room",
-      element: <Room />
-  },
-  {
-      path: "/history",
-      element: <History />
-  },
-  {
-      path: "/building",
-      element: <Building />
-  },
-  {
-    path: "/department",
-    element: <Department />
-  },
-  {
-    path: "/position",
-    element: <Position />
-  },
-  {
-      path: "/status",
-      element: <Status />
-  },
-  {
-      path: "/reserve",
-      element: <Reserve />
-  },
-  {
-      path: "/verify",
-      element: <Verify />
-  },
-  {
-      path: "/report",
-      element: <Report />
-  }
-  ,
-  {
-      path: "/login",
-      element: <Login />
-  }
-  ,
-  {
-      path: "/notifications",
-      element: <Notifications />
-  }
-  ,
-  {
-      path: "/profile",
-      element: <Profile />
-  }
+	{
+		path: "/",
+		element: <Home />
+	},
+	{
+		path: "/user",
+		element: <User />
+	},
+	{
+		path: "/room",
+		element: <Room />
+	},
+	{
+		path: "/history",
+		element: <History />
+	},
+	{
+		path: "/building",
+		element: <Building />
+	},
+	{
+		path: "/department",
+		element: <Department />
+	},
+	{
+		path: "/position",
+		element: <Position />
+	},
+	{
+		path: "/status",
+		element: <Status />
+	},
+	{
+		path: "/reserve",
+		element: <Reserve />
+	},
+	{
+		path: "/mylists",
+		element: <Mylist />
+	},
+	{
+		path: "/booking",
+		element: <Booking />
+	},
+	{
+		path: "/verify",
+		element: <Verify />
+	},
+	{
+		path: "/report",
+		element: <Report />
+	}
+	,
+	{
+		path: "/login",
+		element: <Login />
+	}
+	,
+	{
+		path: "/notifications",
+		element: <Notifications />
+	}
+	,
+	{
+		path: "/profile",
+		element: <Profile />
+	}
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 
