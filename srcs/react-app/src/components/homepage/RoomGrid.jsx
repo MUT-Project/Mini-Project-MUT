bb739407d379e01d92a89181f548c2b6209c7f9b
CornGminimate2P <ftluconcrete@gmail.com>
2024-10-30 05:29:45 +0700

    Link Page

--- srcs/react-app/src/components/homepage/RoomGrid.jsx
+++ srcs/react-app/src/components/homepage/RoomGrid.jsx
@@ -5,57 +5,57 @@ const RoomGrid = () => {
 	return (
 		<div>
 			<p className="bd_room-card-p">Rooms</p>
-			<div className="r_room-grid">
-				<div className="r_room-card">
+			<div className="r_room-grid" >
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building D D504" />
 					<div className="r_room-info">
 						<h3>Building D</h3>
 						<p>D504</p>
 					</div>
 				</div>
-				<div className="r_room-card">
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building F F303" />
 					<div className="r_room-info">
 						<h3>Building F</h3>
 						<p>F303</p>
 					</div>
 				</div>
-				<div className="r_room-card">
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building K K102" />
 					<div className="r_room-info">
 						<h3>Building K</h3>
 						<p>K102</p>
 					</div>
 				</div>
-				<div className="r_room-card">
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building MII MII208" />
 					<div className="r_room-info">
 						<h3>Building MII</h3>
 						<p>MII208</p>
 					</div>
 				</div>
-				<div className="r_room-card">
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building Code:Chicken CC810" />
 					<div className="r_room-info">
 						<h3>Building Code:Chicken</h3>
 						<p>CC810</p>
 					</div>
 				</div>
-				<div className="r_room-card">
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building T-ONE T709" />
 					<div className="r_room-info">
 						<h3>Building T-ONE</h3>
 						<p>T709</p>
 					</div>
 				</div>
-				<div className="r_room-card">
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building SQUARE S106" />
 					<div className="r_room-info">
 						<h3>Building SQUARE</h3>
 						<p>S106</p>
 					</div>
 				</div>
-				<div className="r_room-card">
+				<div className="r_room-card" onClick={() => window.location.href = "/booking"}>
 					<img src={roomImg} alt="Building MIIX MIIX302" />
 					<div className="r_room-info">
 						<h3>Building MIIX</h3>
