ea1999fa31861a8917178b6ee523d85118b272d2
CornGminimate2P <ftluconcrete@gmail.com>
2024-10-31 05:43:38 +0700

    QR UP

---
+++ srcs/react-app/src/components/qr/qr.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./qr.css";
+
+function QR() {
+	return (
+		<>
+			<div className="bgqr">
+				<h3>KUAY</h3>
+				<div className="GAY">
+					<button className="YES">ยืนยัน</button>
+					<button className="NO">ยกเลิก</button>
+				</div>
+			</div >
+		</>
+	);
+}
+
+export default QR;
