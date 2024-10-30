973c675bc79abdbe189aa07bc6af88630da06412
CornGminimate2P <ftluconcrete@gmail.com>
2024-10-31 05:45:48 +0700

    QR UP 2

--- srcs/react-app/src/components/qr/qr.jsx
+++ srcs/react-app/src/components/qr/qr.jsx
@@ -5,7 +5,7 @@ function QR() {
 	return (
 		<>
 			<div className="bgqr">
-				<h3>KUAY</h3>
+				<h3>ยืนยันการเข้าใช้ห้อง</h3>
 				<div className="GAY">
 					<button className="YES">ยืนยัน</button>
 					<button className="NO">ยกเลิก</button>
